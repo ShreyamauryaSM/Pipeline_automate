2117dc65e3b0a3e2bb627a06dc0781e1a4ec9af5
ShreyamauryaSM <shreya.m@softwaremathematics.com>
2024-04-18 10:02:53 +0530

    Done with app.run

--- app.py
+++ app.py
@@ -332,5 +332,5 @@ def index():
 
     return render_template("base.html",company_names=company_names)
 
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
